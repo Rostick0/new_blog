96767cfc2462981c708efc1c94872108ecb9a204
Rostislav <rostik057@gmail.com>
2022-07-10 21:54:40 +0300

    home page

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const {src, dest, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const htmlmin = require('gulp-htmlmin');
+const csso = require('gulp-csso');
+const include = require('gulp-file-include');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const { init, watch, reload } = require('browser-sync').create();
+
+function html() {
+    return src('app/**.html')
+            .pipe(include())
+            .pipe(htmlmin({
+                collapseWhitespace: true
+            }))
+            .pipe(dest('dist'))
+}
+
+function scss() {
+    return src('app/source/scss/style.scss')
+            .pipe(sass())
+            .pipe(autoprefixer({
+                cascade: false
+            }))
+            .pipe(dest('app/css'))
+            .pipe(csso())
+            .pipe(dest('dist/css'))
+}
+
+function js() {
+    return src('app/js/**.js')
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
+            .pipe(uglify())
+            .pipe(dest('dist/js'))
+}
+
+function fonts() {
+    return src('app/fonts/**')
+            .pipe(dest('dist/fonts'))
+}
+
+function img() {
+    return src('app/img/**')
+            .pipe(dest('dist/img'))
+}
+
+function serve() {
+    init({
+        server: './dist'
+    })
+
+    watch('app/**.html', series(html)).on('change', reload);
+    watch('app/source/scss/*.scss', series(scss)).on('change', reload);
+}
+
+exports.build = series(scss, html, js, fonts, img);
+exports.serve = series(scss, html, js, fonts, img, serve);
